84c8f46e2a89a37a9c7194493be58b4dbe490c31
Kendall Rentana <kendallandreyr@gmail.com>
2018-09-18 20:19:52 -0600

    Se creo el primer WorkShop tanto el estatico como el dinamico

---
+++ hello_word1/hello_word.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <?php echo '<h1> Hello Word </h1>' ; ?>
+        <?php print '<img src="img\nissan.jpg" width="800" height="500"';?>
+    </div>
+</body>
+</html>
